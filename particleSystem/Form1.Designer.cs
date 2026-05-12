31b959499faa9de499038ca22ab6656925d8d21f
Александр Сенченко <senchenko.sashuta@list.ru>
2026-05-13 05:51:43 +0800

    Сделал 1 часть по чаинке

--- particleSystem/Form1.Designer.cs
+++ particleSystem/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,15 +38,23 @@
             // 
             this.picDisplay.Location = new System.Drawing.Point(2, 2);
             this.picDisplay.Name = "picDisplay";
-            this.picDisplay.Size = new System.Drawing.Size(797, 446);
+            this.picDisplay.Size = new System.Drawing.Size(1179, 561);
             this.picDisplay.TabIndex = 0;
             this.picDisplay.TabStop = false;
+            this.picDisplay.Click += new System.EventHandler(this.picDisplay_Click);
+            this.picDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDisplay_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1180, 562);
             this.Controls.Add(this.picDisplay);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- particleSystem/Form1.cs
+++ particleSystem/Form1.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static particleSystem.Particle;
 
 namespace particleSystem
 {
     public partial class Form1 : Form
     {
+        Emitter emitter = new Emitter();
+
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.25),
+                Y = picDisplay.Height / 2
+            });
+
+            // в центре антигравитон
+            emitter.impactPoints.Add(new AntiGravityPoint
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            });
+
+            // снова гравитон
+            emitter.impactPoints.Add(new GravityPoint
+            {
+                X = (float)(picDisplay.Width * 0.75),
+                Y = picDisplay.Height / 2
+            });
+        }
+
+        private void picDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState();
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g);
+            }
+            picDisplay.Invalidate();
+        }
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
