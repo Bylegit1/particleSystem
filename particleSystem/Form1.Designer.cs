1eee111f219e89c8b3417b36e10503dadacf67d0
Александр Сенченко <senchenko.sashuta@list.ru>
2026-05-20 08:54:57 +0800

    Добавил Label с информацией для пользователя

--- particleSystem/Form1.Designer.cs
+++ particleSystem/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.picDisplay = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,26 +67,38 @@
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label1.Location = new System.Drawing.Point(10, 523);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(414, 16);
+            this.label1.Size = new System.Drawing.Size(431, 16);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Что добавить счетчик нажмите ПКМ, чтобы удалить тоже ПКМ";
+            this.label1.Text = "Чтобы добавить счетчик нажмите ПКМ, чтобы удалить тоже ПКМ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(10, 539);
+            this.label2.Location = new System.Drawing.Point(10, 507);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(416, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Чтобы добавить радар нажмите ЛКМ, чтобы удалить тоже ЛКМ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(12, 539);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(572, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Чтобы передвигать разноцветные окружности, удерживаете колесико мыши и тащите";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1180, 564);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblDirection);
@@ -104,6 +117,7 @@
         private System.Windows.Forms.PictureBox picDisplay;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
